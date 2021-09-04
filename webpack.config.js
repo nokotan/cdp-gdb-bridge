5c484aad24f8358a879558e51b2bbeb7b780c2b7
nokotan <kamenokonokotan@gmail.com>
2021-09-04 13:42:43 +0900

    add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+
+  entry: './src/vscode/dapServer.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  // entry: './src/cli/index.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'packed'),
+    filename: 'dapServer.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js', '.wasm']
+  },
+  plugins: [
+    new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "crates/dwarf")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    topLevelAwait: true
+  }
+};
+module.exports = config;
